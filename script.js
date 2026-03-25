b33481cad0f7fd1dcc1f050d0373831460cb69e8
joaoConrad <joaoaojthebest2@gmail.com>
2026-03-25 20:37:21 -0300

    adisaum das coisa kk xd

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
-function redirecionar(){
-    window.location.href = "https://store.steampowered.com/app/381210/Dead_by_Daylight/"
+function redirecionar() {
+    window.open(
+        "https://store.steampowered.com/app/381210/Dead_by_Daylight/",
+        "_blank"
+    );
 }
 
-function levarAssassino(){
+function levarAssassino() {
 
-const destino = document.getElementById('assassino_button');
+    const destino = document.getElementById('assassino_button');
 
-window.scrollTo({
-    top: destino.offsetTop,
-    behavior: "smooth"
-});
+    window.scrollTo({
+        top: destino.offsetTop,
+        behavior: "smooth"
+    });
 
 }
