0f8fa5b24586d173c70fa68c08cf2c3332676a89
joaoConrad <joaoaojthebest2@gmail.com>
2026-03-26 02:02:46 -0300

    arquivo

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function redirecionar() {
     );
 }
 
+function home(){
+    window.location.href = "index.html";
+}
+
 function exibirTrailer() {
     const trailer = document.getElementById("trailer");
 
@@ -38,14 +42,7 @@ function fecharVideo() {
 }
 
 function learnMore() {
-
-    const destino = document.getElementById('learnMore');
-
-    window.scrollTo({
-        top: destino.offsetTop,
-        behavior: "smooth"
-    });
-
+    window.location.href = "learnMore.html";
 }
 
 function redirecionarDiscord() {
@@ -62,49 +59,25 @@ function redirecionarReddit() {
     );
 }
 
-const btnSurv = document.getElementById("btnSurv");
-const btnKiller = document.getElementById("btnKiller");
 
-const modalSurv = document.getElementById("modalSurvivor");
-const modalKiller = document.getElementById("modalKiller");
 
-// abrir modais
-btnSurv.addEventListener("click", () => {
-    modalSurv.showModal();
-    descerPagina();
-});
+let index = 0;
 
-btnKiller.addEventListener("click", () => {
-    modalKiller.showModal();
-    descerPagina();
-});
+const texts = document.querySelectorAll(".carousel-text .text");
+const images = document.querySelectorAll(".carousel-images img");
 
-// fechar modal
-function fecharModal(id) {
-    document.getElementById(id).close();
+function trocarSlide() {
+    // remove active
+    texts[index].classList.remove("active");
+    images[index].classList.remove("active");
+
+    // próximo
+    index = (index + 1) % texts.length;
+
+    // adiciona active
+    texts[index].classList.add("active");
+    images[index].classList.add("active");
 }
 
-// scroll automático
-function descerPagina() {
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: "smooth"
-    });
-}
-
-// links das DLCs
-function abrirDLC(personagem) {
-    const links = {
-        Dustin: "https://store.steampowered.com/bundle/67029/Dead_by_Daylight_Stranger_Things_Complete_Edition/",
-        Rick: "https://store.steampowered.com/app/3884020/Dead_by_Daylight_The_Walking_Dead/?curator_clanid=11814497",
-        Leon: "https://store.steampowered.com/app/1634040/Dead_by_Daylight__Resident_Evil_Chapter/?curator_clanid=11814497",
-        Nicolas: "https://store.steampowered.com/app/2469400/Dead_by_Daylight__Nicolas_Cage_Chapter_Pack/?curator_clanid=11814497",
-
-        Vecna: "https://store.steampowered.com/bundle/67029/Dead_by_Daylight_Stranger_Things_Complete_Edition/",
-        Springtrap: "https://store.steampowered.com/bundle/60298/Dead_by_Daylight_Five_Nights_at_Freddys_Edition/",
-        Leatherface: "https://store.steampowered.com/app/700280/Dead_by_Daylight__Leatherface/?curator_clanid=11814497",
-        Krasue: "https://store.steampowered.com/app/4019590/Dead_by_Daylight__Sinister_Grace/?curator_clanid=11814497"
-    };
-
-    window.open(links[personagem], "_blank");
-}
+// troca a cada 3 segundos
+setInterval(trocarSlide, 11000);
